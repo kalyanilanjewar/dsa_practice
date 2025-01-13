13dd921b9015f79062cabd1337a61175d51a1787
Kalyani <kblanjewar024@gmail.com>
2025-01-13 23:32:32 +0530

    find array length

---
+++ dsaWithJava/src/arrays/FindArrayLength.java
@@ -0,0 +1,17 @@
+package arrays;
+
+public class FindArrayLength {
+	
+	public static void main(String[] args)
+	{
+		// Initialize an array
+		int[] arr = {67, 9, 12, 5, 3, 1};
+		
+		// Find the length of the array
+		int length = arr.length;
+		
+		// Print the length of the array
+		System.out.println("Length of the array is: "+length);
+	}
+
+}
