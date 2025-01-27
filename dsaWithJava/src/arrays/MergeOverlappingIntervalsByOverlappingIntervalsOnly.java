b1564d3899427989722cb36b7b3e1c39c746933c
Kalyani <kblanjewar024@gmail.com>
2025-01-27 16:38:16 +0530

    added documentation

--- dsaWithJava/src/arrays/MergeOverlappingIntervalsByOverlappingIntervalsOnly.java
+++ dsaWithJava/src/arrays/MergeOverlappingIntervalsByOverlappingIntervalsOnly.java
@@ -1,3 +1,4 @@
+//Write a java code to merge overlapping intervals by checking overlapping intervals only
 package arrays;
 
 import java.util.ArrayList;
@@ -6,12 +7,13 @@ import java.util.List;
 
 public class MergeOverlappingIntervalsByOverlappingIntervalsOnly {
 	
+	//Function to merge overlapping intervals by checking overlapping intervals only
 	static List<int[]> mergeOverlap(int[][] arr)
 	{
 		//Sort intervals based on start values
 		Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
 		
-		List<int[]> res = new ArrayList<>();
+		List<int[]> res = new ArrayList<>(); //List to store merged intervals
 		res.add(new int[]{arr[0][0], arr[0][1]});
 		
 		for(int i=1; i<arr.length; i++)
@@ -32,17 +34,19 @@ public class MergeOverlappingIntervalsByOverlappingIntervalsOnly {
 		
 		return res;
 	}
+	
+	//Main function to test the code
 	public static void main(String[] args)
 	{
 		int[][] arr = {{7, 8}, {1, 5}, {2, 4}, {4, 6}};
-		List<int[]> res = mergeOverlap(arr);
+		List<int[]> res = mergeOverlap(arr); //Calling function to merge overlapping intervals by checking overlapping intervals only
 		
 		for(int[] interval : res)
 		{
+			//Print the merged intervals after merging overlapping intervals
 			System.out.println(interval[0] + " " + interval[1]);
 		}
 	}
-
 }
 
 /*
