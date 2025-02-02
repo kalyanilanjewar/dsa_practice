33fb254aeae69fa063898074054fddd5bcae1cd1
Kalyani <kblanjewar024@gmail.com>
2025-02-02 19:16:02 +0530

    MergeOverlappingIntervalsInPlace

---
+++ dsaWithJava/src/arrays/MergeOverlappingIntervalsInPlace.java
@@ -0,0 +1,19 @@
+package arrays;
+
+public class MergeOverlappingIntervalsInPlace {
+	
+	public static void main(String[] args)
+	{
+		int[][] arr = {{7, 8}, {1, 5}, {2, 4}, {4, 6}};
+        
+        // Get the new size of the array after merging
+//        int newSize = mergeOverlap(arr);
+//
+//        // Print the merged intervals based on the new size
+//        for (int i = 0; i < newSize; i++) {
+//            System.out.println(arr[i][0] + " " + arr[i][1]);
+//        }
+	}
+
+	
+}
