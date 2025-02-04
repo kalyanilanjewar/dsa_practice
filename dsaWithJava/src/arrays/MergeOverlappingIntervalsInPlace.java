5a93ddddcebfb8c5b751d1db49797869c705fb4b
Kalyani <kblanjewar024@gmail.com>
2025-02-04 22:40:39 +0530

    Merge Overlapping Intervals in-place

--- dsaWithJava/src/arrays/MergeOverlappingIntervalsInPlace.java
+++ dsaWithJava/src/arrays/MergeOverlappingIntervalsInPlace.java
@@ -1,20 +1,53 @@
 // Java Code to Merge Overlapping Intervals in-place
 package arrays;
 
+import java.util.Arrays;
+
 public class MergeOverlappingIntervalsInPlace {
 	
+	// Merge overlapping intervals in-place. We return
+    // modified size of the array arr.
+    static int mergeOverlap(int[][] arr) 
+    {
+        // Sort intervals based on start values
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // Index of the last merged 
+        int resIdx = 0; 
+
+        for (int i = 1; i < arr.length; i++) {
+            
+            // If current interval overlaps with the 
+            // last merged interval
+            if (arr[resIdx][1] >= arr[i][0])           
+                arr[resIdx][1] = Math.max(arr[resIdx][1], arr[i][1]);
+            
+            // Move to the next interval
+            else {            
+                resIdx++;
+                arr[resIdx] = arr[i];
+            }
+        }
+        // Returns size of the merged intervals
+        return (resIdx + 1);
+    }
 	public static void main(String[] args)
 	{
 		int[][] arr = {{7, 8}, {1, 5}, {2, 4}, {4, 6}};
         
         // Get the new size of the array after merging
-//        int newSize = mergeOverlap(arr);
-//
-//        // Print the merged intervals based on the new size
-//        for (int i = 0; i < newSize; i++) {
-//            System.out.println(arr[i][0] + " " + arr[i][1]);
-//        }
-	}
+        int newSize = mergeOverlap(arr);
 
+        // Print the merged intervals based on the new size
+        for (int i = 0; i < newSize; i++) {
+            System.out.println(arr[i][0] + " " + arr[i][1]);
+        }
+	}
 	
 }
+
+/*
+OUTPUT:
+	1 6
+	7 8
+*/
