a3fec5687f6c1a629df94c38a7a4c8ff990aed2f
Kalyani <kblanjewar024@gmail.com>
2025-02-09 22:24:21 +0530

    move all zeros to end using one traversal

---
+++ dsaWithJava/src/arrays/MoveAllZerosToEndUsingOneTraversal.java
@@ -0,0 +1,38 @@
+package arrays;
+
+public class MoveAllZerosToEndUsingOneTraversal {
+	
+	static void pushZerosToEnd(int[] arr)
+	{
+		int count = 0;
+		
+		for(int i=0; i<arr.length; i++)
+		{
+			if(arr[i] != 0)
+			{
+				int temp = arr[i];
+				arr[i] = arr[count];
+				arr[count] = temp;
+				
+				count++;
+			}
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		int[] arr = {1, 2, 0, 4, 3, 0, 5, 0};
+		pushZerosToEnd(arr);
+		
+		for(int num : arr)
+		{
+			System.out.print(num + " ");
+		}
+	}
+
+}
+
+/*
+OUTPUT:
+	1 2 4 3 5 0 0 0
+*/
